3389a55878abcd491da275ebdfaaa2df314da877
Bello <Kakawu@lanciao.com>
2016-11-10 20:54:50 +0800

    Commit

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var sass = require('gulp-sass');
+var bs = require('browser-sync').create();
+
+//Options
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+//Task BrowserSync
+gulp.task('browser-sync', function () {
+    bs.init({
+        server: {
+            baseDir: 'dist',
+            index: "hello.html"
+        }
+    });
+    gulp.watch('./src/jade/*.jade', ['jade']);
+});
+
+//Task jade to HTML
+gulp.task('jade', function() {
+    gulp.src('./src/*.jade')
+    .pipe(jade({
+        pretty: true
+    }))
+    .pipe(gulp.dest('./dist'))
+    .pipe(bs.reload({stream: true}))
+});
+
+//Task Sass to css
+gulp.task('sass', function () {
+    return gulp
+    .src('./src/sass/*.sass')
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(gulp.dest('./dist/style'))
+    .pipe(bs.reload({stream: true}))
+});
+
+//Task watch
+gulp.task('watch', ['browser-sync'], function() {
+    gulp.watch(['./src/*.jade'], ['jade']);
+    gulp.watch('./src/sass/*.sass', ['sass']);
+});
+
+
+//Default task
+gulp.task('default', ['jade', 'sass', 'watch']);
